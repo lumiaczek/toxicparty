c2d5a52922965c5be1a5a6aa600c6c848eb0ad42
Daniel Borowski <danielborowski05@gmail.com>
2024-02-27 19:36:24 +0100

    Zmiana pliku głównego

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <main class="hero h-screen flex justify-center items-center flex-col">
     <img src="favicon.png" alt="">
-    <h1 class="text-color1 text-5xl font-bold mt-10">Już wkrótce</h1>
 </main>
